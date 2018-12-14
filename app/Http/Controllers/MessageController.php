1551e83e28c1de8353b9bffb201f3a22503c6dec
Gbeila Aliu Wahab <aliuwahab@gmail.com>
2018-12-14 08:42:56 +0000

    Implement sending messages and queueing

--- app/Http/Controllers/MessageController.php
+++ app/Http/Controllers/MessageController.php
@@ -2,9 +2,43 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\SendMessageRequest;
+use App\Jobs\SendMessageJob;
+use App\Mail\EmailStaff;
+use App\Message;
+use App\Staff;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Mail;
 
 class MessageController extends Controller
 {
-    //
+
+
+    public function createMessage(Staff $staff){
+         return view('message',compact('staff'));
+    }
+
+
+    public function sendMessage(SendMessageRequest $sendMessageRequest){
+
+        $staff = Staff::findOrFail($sendMessageRequest->get('id'));
+        $create_message = Message::create([
+            'sender_id' => auth()->id(),
+            'subject' => $sendMessageRequest->get('subject'),
+            'content' => $sendMessageRequest->get('content')
+        ]);
+
+
+        SendMessageJob::dispatch($staff,$create_message);
+
+        return "Message sent";
+
+//        return Mail::to($staff)->send(new EmailStaff($staff,$create_message));
+
+    }
+
+
+
+
+
 }
